51ccac3501eab1cf8b2d3ec9c4dd59ef7a21da03
Andrey Ramirez <ramirez.jag00@gmail.com>
2020-09-05 00:43:37 +0800

    Make mobile_token unique and default null

--- models/user.ts
+++ models/user.ts
@@ -1,4 +1,4 @@
-import { Schema, model, PassportLocalSchema } from 'mongoose'
+import { Schema, model, PassportLocalSchema, SchemaTypeOpts } from 'mongoose'
 import passportLocalMongoose from 'passport-local-mongoose'
 
 import UserType from '../types/User'
@@ -6,7 +6,15 @@ import UserType from '../types/User'
 const UserSchema = new Schema({
 	username: { type: String, required: true, unique: true },
   password: { type: String },
-  mobile_token: { type: String, required: false, unique: true },
+  mobile_token: {
+    type: String,
+    required: false,
+    index: ({
+      unique: true,
+      partialFilterExpression: { mobile_token: { $type: 'string' } },
+    } as SchemaTypeOpts.IndexOpts),
+    default: null,
+  }
 })
 
 UserSchema.plugin(passportLocalMongoose)
