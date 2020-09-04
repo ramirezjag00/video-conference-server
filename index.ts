6f5a7be47e95bd74a80023a2532eb76a43670422
Andrey Ramirez <ramirez.jag00@gmail.com>
2020-09-04 16:36:53 +0800

    Add express

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log('HELLO WORLD!')
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Video Conference Server started at http://localhost:${port}`)
+})
