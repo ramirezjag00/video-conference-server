05db2551775a547647e4368b37b7677b3ce32834
Andrey Ramirez <ramirez.jag00@gmail.com>
2020-09-04 17:57:55 +0800

    Set up mongoose

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
 
 const app = express()
 const port = 3000
 
+mongoose.connect('mongodb://localhost/video_conference', { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
